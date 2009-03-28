d5a7620b58b151a582a7d5330b7d6e7979876602
Nick Allen <nick_a@nick-allens-computer.local>
2009-03-28 15:54:07 -0700

    moving incomplete documentation to README.md

--- princ-ut-interval.lisp
+++ princ-ut-interval.lisp
@@ -1,23 +1,6 @@
-;;
-;; todo: -document
-;;
-;;    PRINC-UT-INTERVAL> (princ-ut-interval* (rw-ut:r-ut "2007/1/3")
-;; 					     (rw-ut:r-ut "2008/3/5")
-;; 					     '(:day))
-;;				       
-;;    "427 days"
-;;
-;;    PRINC-UT-INTERVAL> (princ-ut-interval* (rw-ut:r-ut "2007/1/3")
-;; 					     (rw-ut:r-ut "2008/3/5")
-;; 					     '(:week :day))
-;;				       
-;;    "61 weeks, 0 days"
-;;
-;;    PRINC-UT-INTERVAL> (princ-ut-interval* (rw-ut:r-ut "2007/1/3")
-;; 					     (rw-ut:r-ut "2008/3/5")
-;; 					     '(:year :week :day))
-;;				       
-;;   "1 year, 8 weeks, 6 days"
+;
+; http://github.com/nallen05/princ-ut-interval
+;
 
 (defpackage :princ-ut-interval
   (:use :cl)
